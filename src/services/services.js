d7e4f2bfa837d9933944fb33bc864c0cb3af9976
Arguish <jhergon8@gmail.com>
2023-07-02 20:24:23 +0100

    wip

--- src/services/services.js
+++ src/services/services.js
@@ -5,13 +5,12 @@ export const register = async (nick, mail, pass) => {
     const userList = await axios.get(
       "https://todo-team.onrender.com/api/user/all"
     );
-    console.log(userList.data);
 
     if (
       userList.data.some((user) => user.nick === nick) ||
       userList.data.some((user) => user.mail === mail)
     ) {
-      console.log("nick o mail repetido");
+      alert("nick o mail en uso");
     } else {
       if (nick.includes("Admin")) {
         const userCreate = await axios.post(
@@ -23,9 +22,7 @@ export const register = async (nick, mail, pass) => {
           "https://todo-team.onrender.com/api/user/add",
           { nick, mail, pass }
         );
-        console.log(userCreate);
       }
-      alert("Usuario creado (de verdad)");
       return true;
     }
   } catch (error) {
@@ -39,11 +36,33 @@ export const loggin = async (mail, pass) => {
       "https://todo-team.onrender.com/api/user/loggin",
       { mail, pass }
     );
-    console.log(userLog);
-    console.log(userLog.data.canLog);
     localStorage.setItem("User", JSON.stringify(userLog.data.result));
 
     return userLog.data.canLog;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getUserGroups = async (user) => {
+  try {
+    const response = await axios.post(
+      "https://todo-team.onrender.com/api/group/user/",
+      user
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteGroup = async (id) => {
+  try {
+    const response = await axios.delete(
+      `https://todo-team.onrender.com/api/group/${id}`
+    );
+    console.log(response);
   } catch (error) {
     console.error(error);
   }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,11 +9,31 @@
         class="flex flex-col-reverse justify-between w-1/5 border-orange-100 border-2 rounded-xl m-4"
       >
         <button
+          @click=""
           class="m-2 px-4 bg-slate-500 rounded-full hover:bg-slate-600 active:bg-slate-400"
         >
           Añadir grupo
         </button>
-        grupos
+        <div>
+          <h1>---V--- Grupos ---V---</h1>
+          <div
+            id="groupItem"
+            v-for="(item, index) in userGroups"
+            class="relative flex justify-center items-center hover:bg-slate-950 transition-all"
+          >
+            <h1
+              class="text-sm text-center border-2 border-orange-100 rounded-md p-2"
+            >
+              {{ item._id }}
+            </h1>
+            <button
+              @click="() => deleteThisGroup($event, item._id)"
+              class="absolute right-1 px-1 text-center border-2 border-red-500 text-red-500 bg-red-500/40 rounded-full"
+            >
+              X
+            </button>
+          </div>
+        </div>
       </div>
       <div class="w-4/5 border-orange-100 border-2 rounded-xl m-4">Todos</div>
     </div>
@@ -21,29 +41,40 @@
 </template>
 
 <script>
-import { register, loggin } from "../services/services.js";
+import { getUserGroups, deleteGroup } from "../services/services.js";
 export default {
   name: "Main",
   data: () => ({
     user: {},
     userGroups: [],
+    activeGroup: "",
   }),
   methods: {
+    async newToDo() {},
+
     setLocalUser() {
       const userdata = JSON.parse(localStorage.getItem("User"));
-      console.log(userdata);
       this.user = userdata;
     },
-
-    async getUserGroups(id) {
-      console.log(id);
-    },
   },
-  created() {
+  async created() {
     this.setLocalUser();
-    this.getUserGroups(this.user._id);
+    this.userGroups = await getUserGroups(this.user);
+  },
+  async deleteThisGroup(ev, id) {
+    deleteGroup(id);
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+#groupItem > button {
+  visibility: collapse;
+}
+#groupItem:hover > h1 {
+  opacity: 40%;
+}
+#groupItem:hover > button {
+  visibility: visible;
+}
+</style>
